ad6261c663af11773a5267694e2bec5ec53f145b
jcasahuamang <jcasahuamang@gmail.com>
2025-07-07 21:48:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [Author](#author)
+
+## Overview
+
+### Links
+
+- Solution URL: [Add solution URL here](https://jcasahuamang.github.io/fmentor-time-tracking.git)
+- Live Site URL: [Add live site URL here](https://jcasahuamang.github.io/fmentor-time-tracking)
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/jcasahuamang)
---
+++ assets/js/index.js
@@ -0,0 +1,69 @@
+import data from '../data/data.json' with { type: 'json' };
+const container = document.getElementById('dashboard');
+
+const appendItem = (item) => {
+    container.innerHTML += `
+  <section class="${(item.title).toLowerCase()} card">
+          <div class="background layout background-${item.background}">
+            <img src="../../images/icon-${(item.title).toLowerCase()}.svg" alt="" class="background-img"/>
+          </div>
+          <div class="layout">
+            <p class="medium-text">${item.title}</p>
+            <img src="../../images/icon-ellipsis.svg" alt="More options" width="16" class="options"/>
+          </div>
+          <div class="layout hours">
+            <p  class="big-text daily hidden">${item.timeframes.daily.current}hrs</p>
+            <p  class="big-text weekly">${item.timeframes.weekly.current}hrs</p>
+            <p  class="big-text monthly hidden">${item.timeframes.monthly.current}hrs</p>
+            <p  class="small-text daily hidden">Last Week - ${item.timeframes.daily.previous}hrs</p>
+            <p  class="small-text weekly">Last Week - ${item.timeframes.weekly.previous}hrs</p>
+            <p  class="small-text monthly hidden">Last Week - ${item.timeframes.monthly.previous}hrs</p>            
+          </div>
+        </section>
+    `;
+  }
+  
+
+  data.forEach((item) => {
+      appendItem(item);
+  });
+
+const daily = document.querySelectorAll(".daily")
+const weekly = document.querySelectorAll(".weekly")
+const monthly = document.querySelectorAll(".monthly")
+
+const dailyButton = document.getElementById("daily-btn");
+const weeklyButton = document.getElementById("weekly-btn");
+const monthlyButton = document.getElementById("monthly-btn");
+const buttons = [dailyButton,weeklyButton,monthlyButton]
+
+
+buttons.map((button) => button.addEventListener("click", () => 
+{ if (button.innerHTML === "Daily"){
+    
+    daily.forEach(item => item.classList.remove("hidden"));
+    weekly.forEach(item => item.classList.add("hidden"));
+    monthly.forEach(item => item.classList.add("hidden"));
+
+    dailyButton.style.color="white"
+    weeklyButton.style.color="inherit"
+    monthlyButton.style.color="inherit"
+} else if (button.innerHTML === "Weekly") {
+    daily.forEach(item => item.classList.add("hidden"));
+    weekly.forEach(item => item.classList.remove("hidden"));
+    monthly.forEach(item => item.classList.add("hidden"));
+    dailyButton.style.color="inherit"
+    weeklyButton.style.color="white"
+    monthlyButton.style.color="inherit"
+} else {
+    daily.forEach(item => item.classList.add("hidden"));
+    weekly.forEach(item => item.classList.add("hidden"));
+    monthly.forEach(item => item.classList.remove("hidden"));
+    dailyButton.style.color="inherit"
+    weeklyButton.style.color="inherit"
+    monthlyButton.style.color="white"
+}
+}
+))
+
+
